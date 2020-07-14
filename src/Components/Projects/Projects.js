a10cf967faf888a6c2ad40027941ab88abc01945
Quinton H Brown <quintonbrown8101@yahoo.com>
2020-07-14 16:52:55 -0400

    font size

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -5,19 +5,19 @@ const projects = require('../../data/projects.json')
 const Projects = () => {
     return (
         <div className="project-content" id="projects">
-            <div className="project-header">Check out some of my work!</div>
+            <div className="project-header">Highlights of my work!</div>
         <div className="projects-wrapper">
             {projects.map((project) => {
                 return (
                     <div className="projects-card-wrapper" key={project.title}>
                     
-                        <img className="cover" src={project.cover} alt={project.title} />
+                        <img className="highlight-cover" src={project.cover} alt={project.title} />
                         
                             <div className="middle">
-                            <h2 className="title">{project.title}</h2>
-                            <p className="description">{project.description}</p>
-                            <a className="url" href={project.url} target="_blank" rel="noopener noreferrer">Website</a>
-                            <a className="url" href={project.github} target="_blank" rel="noopener noreferrer">Github</a>
+                            <h2 className="highlight-title">{project.title}</h2>
+                            <p className="highlight-description">{project.description}</p>
+                            <a className="highlight-url" href={project.url} target="_blank" rel="noopener noreferrer">Website</a>
+                            <a className="highlight-url" href={project.github} target="_blank" rel="noopener noreferrer">Github</a>
                         </div>
                         </div>
                   
